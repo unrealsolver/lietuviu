997492fcc25cbc309bebccc5909f94cc04e8af43
Ruslan Panasiuk <ruslan.panasiuk@protonmail.com>
2026-02-13 03:49:32 +0200

    fix: routing

--- packages/cardsapp/src/App.tsx
+++ packages/cardsapp/src/App.tsx
@@ -3,7 +3,7 @@ import { HashRouter, Route, Routes, Navigate } from "react-router";
 
 export default function App() {
   return (
-    <HashRouter basename={import.meta.env.BASE_URL}>
+    <HashRouter /* For browser router: basename={import.meta.env.BASE_URL} */>
       <Routes>
         <Route index element={<Navigate to="/cards/" />} />
         <Route path="/cards" element={<CardsPage />} />
